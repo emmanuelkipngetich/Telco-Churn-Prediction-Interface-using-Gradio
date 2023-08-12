c91927e4d27e9ea4388cc5311ca11c2783753105
Newton23-nk <newtonkimathi20@gmail.com>
2023-08-12 17:44:53 +0300

    Churn Prediction Gradio App

--- Assets/app.py
+++ Assets/app.py
@@ -1,31 +1,99 @@
+# Importations
+import pandas as pd
+import gradio as gr
+import os
+import pickle
+
+
+# Creating key List
+expected_inputs = ['SeniorCitizen', 'tenure', 'MonthlyCharges', 'TotalCharges',
+                   'gender', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines',
+                   'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
+                   'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract',
+                   'PaperlessBilling', 'PaymentMethod']
+
+
 # Function to load machine learning components
 def load_components_func(fp):
-    #To load the machine learning components saved to re-use in the app
-    with open(fp,"rb") as f:
-        object = pickle.load(f)
-    return object
+    # To load the machine learning components saved to re-use in the app
+    with open(fp, "rb") as f:
+        obj = pickle.load(f)
+    return obj
+
 
 # Loading the machine learning components
 DIRPATH = os.path.dirname(os.path.realpath(__file__))
-ml_core_fp = os.path.join(DIRPATH,"Assets","ML_Model.pkl")
+ml_core_fp = os.path.join(DIRPATH, "ML_Model.pkl")
 ml_components_dict = load_components_func(fp=ml_core_fp)
 
-#Unpacking my components
+# Unpacking my components
 label_encoder = ml_components_dict['label_encoder']
 encoder = ml_components_dict['encoder']
-imputer =ml_components_dict['imputer']
+imputer = ml_components_dict['imputer']
 scaler = ml_components_dict['scaler']
+balance = ml_components_dict['imbalance']
 model = ml_components_dict['model']
 
 
+def predict_churn(*args, scaler=scaler, model=model, imputer=imputer, encoder=encoder):
+
+    input_data = pd.DataFrame([args], columns=expected_inputs)
+
+    # Encode the data
+    num_col = input_data[['SeniorCitizen', 'tenure', 'MonthlyCharges', 'TotalCharges']]
+    cat_col = input_data[['gender', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines',
+       'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
+       'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract',
+       'PaperlessBilling', 'PaymentMethod']]
+    cat_col = cat_col.astype(str)
+    encoded_data = encoder.transform(cat_col)
+    encoded_df = pd.concat([num_col, encoded_data], axis=1)
+
+    # Imputing missing values
+    imputed_df = imputer.transform(encoded_df)
+
+    # Scaling
+   # columns_to_scale = ['tenure', 'MonthlyCharges', 'TotalCharges','Contract_One year',
+       #                 'Contract_Two year','Dependents_Yes','DeviceProtection_Yes','InternetService_Fiber optic']
+    #scaled_df = imputed_df.copy()
+    scaled_df = scaler.transform(encoded_df)
+
+    # Prediction
+    model_output = model.predict_proba(scaled_df)
+    prob_Churn = float(model_output[0][0])
+    prob_Stay = 1 - prob_Churn
+    return {"Prediction Churn": prob_Churn,
+            "Prediction Not Churn": prob_Stay}
 
 
+# Function to process inputs and return prediction
+Gender = gr.Radio(choices=['Male', 'Female'], label="Gender")
+Partner = gr.Radio(choices=['Yes', 'No'], label="Partner")
+Dependents = gr.Radio(choices=['Yes', 'No'], label="Dependents")
+Tenure = gr.Number(label="Tenure")
+InternetService = gr.Radio(choices=['DSL', 'Fiber optic', 'No Internet Service'], label="Internet Service")
+PhoneService = gr.Radio(choices=['Yes', 'No'], label="Phone Service")
+MultipleLines = gr.Radio(choices=['Yes', 'No'], label="Multiple Lines")
+Contract = gr.Radio(choices=['Month-to-month', 'One year', 'Two year'], label="Contract")
+MonthlyCharges = gr.Number(label="Monthly Charges")
+TotalCharges = gr.Number(label="Total Charges")
+PaperlessBilling = gr.Radio(choices=['Yes', 'No'], label='Paperless Billing')
+PaymentMethod = gr.Radio(choices=['Electronic check', 'Mailed check', 'Bank transfer (automatic)',
+                                'Credit card (automatic)'], label="Payment Method")
+OnlineSecurity = gr.Radio(choices=['Yes', 'No'], label="Online Security")
+OnlineBackup = gr.Radio(choices=['Yes', 'No', 'None'], label="Online Backup")
+DeviceProtection = gr.Radio(choices=['Yes', 'No'], label="Device Protection")
+TechSupport = gr.Radio(choices=['Yes', 'No'], label="Tech Support")
+StreamingTV = gr.Radio(choices=['Yes', 'No'], label="Streaming TV")
+SeniorCitizen = gr.Radio(choices=[0, 1], label='Senior Citizen')
+StreamingMovies = gr.Radio(choices=['Yes', 'No'], label="Streaming Movies")
 
-components = {
-    'label_encoder': label_encoder,
-    'encoder': encoder,
-    'scaler': scaler,
-    'imbalance': smote,
-    'grid_search_rfc':grid_search_rf,
-    'model': rf_model_tuned_Sn
-}
+# Output
+gr.Interface(inputs=[SeniorCitizen, Tenure, MonthlyCharges, TotalCharges,
+                     Gender, Partner, Dependents, PhoneService, MultipleLines,
+                     InternetService, OnlineSecurity, OnlineBackup, DeviceProtection,
+                     TechSupport, StreamingTV, StreamingMovies, Contract,
+                     PaperlessBilling, PaymentMethod],
+             outputs=gr.Label("Awaiting Submission...."),
+             fn=predict_churn,
+             title="App to predict Customer Churn").launch(inbrowser=True, show_error=True, share=True)
