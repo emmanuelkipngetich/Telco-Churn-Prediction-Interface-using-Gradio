3f8666ae8c87e9bbea0c1712e282f514f98107b6
emmanuel kipngetich <kingmanmtahi@gmail.com>
2023-08-14 23:15:23 +0300

    Made Changes

--- Assets/app.py
+++ Assets/app.py
@@ -20,3 +20,24 @@ def load_components_func(fp):
         object = pickle.load(f)
     return object
 
+#Loading the machine learning components
+def load_components_func(fp):
+    #To load the machine learning components to re-use in the app
+    with open(fp, "rb") as f:
+        object = pickle.load(f)
+    return object
+
+# Loading the machine learning components 
+DIRPATH = os.path.dirname(os.path.realpath(__file__))
+ml_core_fp = os.path.join(DIRPATH, "ML_Model.pkl")
+ml_components_dict = load_components_func(fp=ml_core_fp)
+
+#Unpacking my components
+label_encoder = ml_components_dict['label_encoder']
+encoder = ml_components_dict['encoder']
+imputer = ml_components_dict['imputer']
+scaler = ml_components_dict['imbalance']
+model = ml_components_dict['model']
+
+def predict_churn(*args, scaler=scaler, model=model, imputer=imputer, encoder=encoder):
+    input_data = pd.DataFrame([args], columns=expected_inputs)
