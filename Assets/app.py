b00af6bbad9c46f29dea60dbdb46bbbed5026aad
emmanuel kipngetich <kingmanmtahi@gmail.com>
2023-08-15 21:01:34 +0300

    Made Changes

--- Assets/app.py
+++ Assets/app.py
@@ -58,3 +58,35 @@ def predict_churn(*args, scaler=scaler, model=model, imputer=imputer, encoder=en
     # Scaling
    
     scaled_df = scaler.transform(encoded_df)
+
+# Prediction
+    model_output = model.predict_proba(scaled_df)
+    #Probability of Churn(Positive class)
+    prob_Churn = float(model_output[0][1]) 
+    #Probability of staying(Negative Class)
+    prob_Stay = 1 - prob_Churn
+    return {"Prediction Churn": prob_Churn,
+            "Prediction Not Churn": prob_Stay}
+
+
+# We define our inputs
+Gender = gr.Radio(choices=['Male', 'Female'], label="Gender : Gender of the customer")
+Partner = gr.Radio(choices=['Yes', 'No'], label="Partner : Whether the customer has a partner.")
+Dependents = gr.Radio(choices=['Yes', 'No'], label="Dependents : Whether the customer has dependents.")
+Tenure = gr.Number(label="Tenure : The Number of months the customer has been with the company.")
+InternetService = gr.Radio(choices=['DSL', 'Fiber optic', 'No'], label="Internet Service : Type of internet service.")
+PhoneService = gr.Radio(choices=['Yes', 'No'], label="Phone Service : Whether the customer has phone service.")
+MultipleLines = gr.Radio(choices=['Yes', 'No'], label="Multiple Lines : Whether the customer has multiple phone lines.")
+Contract = gr.Radio(choices=['Month-to-month', 'One year', 'Two year'], label="Contract : Type of contract the customer has.")
+MonthlyCharges = gr.Number(label="Monthly Charges : Amount of monthly charges.")
+TotalCharges = gr.Number(label="Total Charges : Total amount charged to the customer.")
+PaperlessBilling = gr.Radio(choices=['Yes', 'No'], label='Paperless Billing : Whether the customer uses paperless billing.')
+PaymentMethod = gr.Radio(choices=['Electronic check', 'Mailed check', 'Bank transfer (automatic)',
+                                'Credit card (automatic)'], label="Payment Method : Payment method used by the customer.")
+OnlineSecurity = gr.Radio(choices=['Yes', 'No'], label="Online Security : Whether the customer has online security service.")
+OnlineBackup = gr.Radio(choices=['Yes', 'No', 'None'], label="Online Backup : Whether the customer has online backup service.")
+DeviceProtection = gr.Radio(choices=['Yes', 'No'], label="Device Protection : Whether the customer has device protection service.")
+TechSupport = gr.Radio(choices=['Yes', 'No'], label="Tech Support : Whether the customer has tech support service.")
+StreamingTV = gr.Radio(choices=['Yes', 'No'], label="Streaming TV : Whether the customer uses streaming TV service.")
+SeniorCitizen = gr.Radio(choices=[0, 1], label='Senior Citizen : Whether the customer is a senior citizen(0 for No and 1 For Yes).')
+StreamingMovies = gr.Radio(choices=['Yes', 'No'], label="Streaming Movies : Whether the customer uses streaming movies service.")
